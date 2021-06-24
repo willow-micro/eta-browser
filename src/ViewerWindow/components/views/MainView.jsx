e5cd71051507fccd1c2d29572f7ed1de4549d826
Takuma Kawamura <noka@willow-micro.com>
2021-06-24 18:22:51 +0900

    use callback ref instead of useRef and useEffect normally

--- src/ViewerWindow/components/views/MainView.jsx
+++ src/ViewerWindow/components/views/MainView.jsx
@@ -1,42 +1,28 @@
 // System
-import React, { useCallback, useState, useRef, useEffect } from 'react';
+import React, { useCallback, useState, useRef } from 'react';
 // User
 import Capturer from '../../Capturer.js';
 
-// Sub Component
-const WebView = React.forwardRef((props, ref) => {
-    return (
-        <React.Fragment>
-          {/* WebView */}
-          {/* See: Preload attribute needs absolute path */}
-          {
-              ( props.url !== "" ) && <webview preload="file:///Users/kawa/Wakayama/2021/HCDLab/electron/eta-browser/src/ViewerWindow/components/views/WebView/DomAnalyzer.js"
-                                               ref={ref}
-                                               src={props.url}
-                                               style={{ height: '100%', width: '100%' }}
-                                               nodeintegration="true"
-                                               nodeintegrationinsubframes="true" />
-          }
-        </React.Fragment>
-    )
-});
 
+// Use callback ref instread of useEffect and useRef normally
+// Because useEffect does not work with ref changes properly
+// See (official): https://ja.reactjs.org/docs/hooks-faq.html#how-can-i-measure-a-dom-node
+// See: https://medium.com/@teh_builder/ref-objects-inside-useeffect-hooks-eb7c15198780
+function useHookWithRefCallback() {
+    // "ref" is in the useHookWithRefCallback function object
+    // So you can specify cleanup processes
+    const ref = useRef(null);
+    // Callback ref
+    const setRef = useCallback((node) => {
+        if (ref.current) {
+            // Make sure to cleanup any events/references added to the last instance
+            ref.current.removeEventListener("ipc-message");
+        }
 
-// Main Component
-const MainView = () => {
-
-    const capturer = new Capturer("ETA Browser (Viewer)", "SaveBufferToFile");
-
-    // useState
-    const [viewerUrl, setViewerUrl] = useState("");
-
-    // useRef
-    const webViewEl = useRef(null);
-
-    // useEffect
-    useEffect(() => {
-        if (webViewEl && webViewEl.current) {
-            webViewEl.current.addEventListener("ipc-message", (event) => {
+        if (node) {
+            // Check if a node is actually passed. Otherwise node would be null.
+            // You can now do what you need to, addEventListeners, measure, etc.
+            node.addEventListener("ipc-message", (event) => {
                 switch (event.channel) {
                     case "SendDOMDataFromWebViewToViewer":
                         console.log(event.args[0].coordinates.x + ", " + event.args[0].coordinates.y);
@@ -60,12 +46,45 @@ const MainView = () => {
                         console.log("ch: " + event.channel + ", args: " + event.args);
                 }
             });
-
-            return () => {
-                webViewEl.current.removeEventListener("ipc-message");
-            }
         }
-    });
+
+        // Save a reference to the node
+        ref.current = node
+    }, []);
+
+    return [ ref, setRef ];
+}
+
+
+// Sub Component
+const WebView = React.forwardRef((props, ref) => {
+    if (props.url === "") {
+        return (null);
+    } else {
+        // Attension: Preload attribute needs absolute path
+        return (
+            <webview preload="file:///Users/kawa/Wakayama/2021/HCDLab/electron/eta-browser/src/ViewerWindow/components/views/WebView/DomAnalyzer.js"
+                     ref={ ref }
+                     src={ props.url }
+                     style={ { height: '100%', width: '100%' } }
+                     nodeintegration="true"
+                     nodeintegrationinsubframes="true" />
+        );
+    }
+});
+
+// Main Component
+const MainView = () => {
+
+    const capturer = new Capturer("ETA Browser (Viewer)", "SaveBufferToFile");
+
+    // useState
+    const [viewerUrl, setViewerUrl] = useState("");
+
+    // useRef
+    const [webViewRefObj, webViewRef] = useHookWithRefCallback();
+
+    // useEffect
 
 
     // IPC Message Rx (from Main)
@@ -75,14 +94,14 @@ const MainView = () => {
     });
     window.viewerIPCOn("OpenDevTools", (event, arg) => {
         console.log("OpenDevTools");
-        if (webViewEl && webViewEl.current && !webViewEl.current.isDevToolsOpened()) {
-            webViewEl.current.openDevTools();
+        if (webViewRefObj.current && !webViewRefObj.current.isDevToolsOpened()) {
+            webViewRefObj.current.openDevTools();
         }
     });
     window.viewerIPCOn("SendDataFromViewerToWebView", (event, arg) => {
         console.log("Data for webview: " + arg.data);
-        if (webViewEl && webViewEl.current) {
-            webViewEl.current.send(
+        if (webViewRefObj.current) {
+            webViewRefObj.current.send(
                 "SendDataFromViewerToWebView",
                 {
                     data: arg.data
@@ -101,7 +120,7 @@ const MainView = () => {
 
     // JSX
     return (
-        <WebView url={viewerUrl} ref={webViewEl} />
+        <WebView url={ viewerUrl } ref={ webViewRef } />
     );
 };
 
