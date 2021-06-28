97b5ce699443a0939f9c4c678164e76089245d36
Takuma Kawamura <noka@willow-micro.com>
2021-06-28 16:38:23 +0900

    Target element filtering: Now the deepest matched element is shown in the main window

--- src/ViewerWindow/components/views/WebView/DomAnalyzer.js
+++ src/ViewerWindow/components/views/WebView/DomAnalyzer.js
@@ -21,12 +21,31 @@ document.addEventListener('DOMContentLoaded', function () {
 // Mousemove event
 // Debounce with 150ms
 document.addEventListener('mousemove', debounce(150, false, (event) => {
-    let elem = document.elementFromPoint(event.clientX, event.clientY);
+    // Deeper Elements First
+    const elements = document.elementsFromPoint(event.clientX, event.clientY);
 
-    console.log("type: " + elem.tagName);
-    console.log("id: " + elem.id);
-    console.log("role: " + elem.getAttribute("role"));
-    console.log("aria-label: " + elem.ariaLabel);
+    // Serach for Sectioning Contents and WAI-ARIA Contents
+    const targetTagNames = ["address", "article", "aside", "footer", "header", "h1", "h2", "h3", "h4", "h5", "h6", "main", "nav", "section"];
+    const targetAttributes = ["role", "aria-label"];
+    const filteredElements = elements.filter(element => {
+        // TagName is matched
+        if (targetTagNames.includes(element.tagName)) {
+            return true;
+        }
+        // Has specified attributes
+        for (let i = 0; i < targetAttributes.length; i++) {
+            if (element.hasAttribute(targetAttributes[i])) {
+                return true;
+            }
+        }
+        return false;
+    });
+
+    // The Deepest Matched Element
+    console.log("type: " + filteredElements[0].tagName);
+    console.log("id: " + filteredElements[0].id);
+    console.log("role: " + filteredElements[0].getAttribute("role"));
+    console.log("aria-label: " + filteredElements[0].ariaLabel);
 
     ipcRenderer.sendToHost(
         "SendDOMDataFromWebViewToViewer",
@@ -35,9 +54,9 @@ document.addEventListener('mousemove', debounce(150, false, (event) => {
                 x: event.clientX,
                 y: event.clientY
             },
-            tagName: elem.tagName,
-            id: elem.id,
-            role: elem.getAttribute("role"),
-            ariaLabel: elem.ariaLabel
+            tagName: filteredElements[0].tagName,
+            id: filteredElements[0].id,
+            role: filteredElements[0].getAttribute("role"),
+            ariaLabel: filteredElements[0].ariaLabel
         });
 }));
